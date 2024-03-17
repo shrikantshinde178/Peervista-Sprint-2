b8c066293d13f23e5e7ed5c3b08e78b867d4a83e
Shrikant Shinde <110349806+shrikantshinde178@users.noreply.github.com>
2024-03-17 13:30:18 +0530

    Delete Backend/peervista.com directory

--- Backend/peervista.com/src/main/java/com/peervista/exceptionHandler/PolicyApplicationException.java
+++
@@ -1,7 +0,0 @@
-package com.peervista.exceptionHandler;
-
-public class PolicyApplicationException extends RuntimeException {
-    public PolicyApplicationException(String message) {
-        super(message);
-    }
-}
--- Backend/peervista.com/src/main/java/com/peervista/exceptionHandler/UnauthorizedUpdateException.java
+++
@@ -1,5 +0,0 @@
-package com.peervista.exceptionHandler;
-
-public class UnauthorizedUpdateException {
-
-}
